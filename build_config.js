b4a96d0d9516f5f24a0edbc74af45983a1674f1d
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-01-11 23:16:20 +0000

    Add discord counts

---
+++ build_config.js
@@ -0,0 +1,34 @@
+'use strict';
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const token = process.env.DISCORD_TOKEN
+var config = {};
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.guilds.fetch("278589367641964544")
+        .then(guild => {
+            config["member_count"] = guild.memberCount;
+            guild.fetchBans()
+                .then(bans => {
+                    console.log();
+                    client.destroy()
+                    config["ban_count"] = bans.size;
+                    const json = JSON.stringify(config);
+                    fs.writeFile('js/config.json', json, err => {
+                        if (err) {
+                            console.log('Error writing file', err)
+                        } else {
+                            console.log('Successfully wrote file')
+                        }
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+client.login(token);
---
+++ js/index.js
@@ -0,0 +1,8 @@
+const config = require('./config.json')
+console.log(config);
+
+const member_count = document.getElementById("member_count")
+member_count.innerHTML = config.member_count
+
+const ban_count = document.getElementById("ban_count")
+ban_count.innerHTML = config["ban_count"]
